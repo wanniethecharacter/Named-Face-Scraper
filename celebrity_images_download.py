71d31bf151b1572fa3c4aa49a7c8e58cdc0f7705
thejester129 <thejester129@gmail.com>
2022-12-23 17:03:45 +0000

    some changes

--- celebrity_images_download.py
+++ celebrity_images_download.py
@@ -1,7 +1,3 @@
-# Author: Prateek Mehta
-# Script to download images from google
-
-
 import time  # Importing the time library to check the time of code execution
 import sys  # Importing the System Library
 import os
@@ -12,7 +8,13 @@ from bs4 import BeautifulSoup
 ########### Edit From Here ###########
 
 # This list is used to search keywords. You can edit this list to search for google images of your choice. You can simply add and remove elements of the list.
-search_keyword = ['Tom Hanks']
+name_list = open("name_list", "r")
+names = name_list.readlines()
+names_to_search = []
+for name in names:
+    names_to_search.append(name.strip("\n"))
+
+names_to_search = ["John"]
 
 # This list is used to further add suffix to your search term. Each element of the list will help you download 100 images. First element is blank which denotes that no suffix is added to the search keyword of the above list. You can edit the list by adding/deleting elements from it.So if the first element of the search_keyword is 'Australia' and the second element of keywords is 'high resolution', then it will search for 'Australia High Resolution'
 keywords = [' face',
@@ -28,7 +30,6 @@ keywords = [' face',
 
 # Downloading entire Web Document (Raw Page Content)
 def download_page(url):
-    # urllib library for Extracting web pages
     try:
         headers = {}
         headers['User-Agent'] = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
@@ -39,39 +40,15 @@ def download_page(url):
     except Exception as e:
         print(str(e))
 
-
-# Finding 'Next Image' from the given raw page
-def _images_get_next_item(s):
-    start_line = s.find('rg_di')
-    if start_line == -1:  # If no links are found then give an error!
-        end_quote = 0
-        link = "no_links"
-        return link, end_quote
-    else:
-        start_line = s.find('"class="rg_meta"')
-        start_content = s.find('"ou"', start_line+1)
-        end_content = s.find(',"ow"', start_content+1)
-        content_raw = str(s[start_content+6:end_content-1])
-        return content_raw, end_content
-
-
 # Getting all links with the help of '_images_get_next_image'
+
+
 def _images_get_all_items(page):
     items = []
     soup = BeautifulSoup(page, 'html.parser')
     imgs = soup.find_all('img')
     for img in imgs:
         items.append(img.get('src'))
-    # while True:
-    #     item, end_content = _images_get_next_item(page)
-    #     if item == "no_links":
-    #         break
-    #     else:
-    #         # Append all the links in the list named 'Links'
-    #         items.append(item)
-    #         # Timer could be used to slow down the request for image downloads
-    #         time.sleep(0.1)
-    #         page = page[end_content:]
     return items
 
 
@@ -80,19 +57,19 @@ t0 = time.time()  # start the timer
 
 # Download Image Links
 i = 0
-while i < len(search_keyword):
+while i < len(names_to_search):
     items = []
     iteration = "Item no.: " + \
-        str(i+1) + " -->" + " Item name = " + str(search_keyword[i])
+        str(i+1) + " -->" + " Item name = " + str(names_to_search[i])
     print(iteration)
     print("Evaluating...")
-    search_keywords = search_keyword[i]
+    search_keywords = names_to_search[i]
     search = search_keywords.replace(' ', '%20')
 
     # make a search keyword  directory
     try:
-        os.chdir("faces/")
-        os.makedirs(search_keywords)
+        # os.chdir("faces/")
+        os.mkdir("faces/" + names_to_search[i])
     except OSError as e:
         if e.errno != 17:
             raise
@@ -113,11 +90,11 @@ while i < len(search_keyword):
     print("\n")
 
     # This allows you to write all the links into a test file. This text file will be created in the same directory as your code. You can comment out the below 3 lines to stop writing the output to the text file.
-    info = open('output.txt', 'a')  # Open the text file called database.txt
-    # Write the title of the page
-    info.write(str(i) + ': ' +
-               str(search_keyword[i-1]) + ": " + str(items) + "\n\n\n")
-    info.close()  # Close the file
+    # info = open('output.txt', 'a')  # Open the text file called database.txt
+    # # Write the title of the page
+    # info.write(str(i) + ': ' +
+    #            str(search_keyword[i-1]) + ": " + str(items) + "\n\n\n")
+    # info.close()  # Close the file
 
     t1 = time.time()  # stop the timer
     # Calculating the total time required to crawl, find and download all the links of 60,000 images
@@ -136,8 +113,7 @@ while i < len(search_keyword):
             req = Request(items[k], headers={
                           "User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
             response = urlopen(req, None, 15)
-            print(os.getcwd())
-            file_name = search_keywords + "/"+str(k+1)+".jpg"
+            file_name = "faces/" + search_keywords + "/"+str(k+1)+".jpg"
             output_file = open(file_name, "wb")
 
             data = response.read()
